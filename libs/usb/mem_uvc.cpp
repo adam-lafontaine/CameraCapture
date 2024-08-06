0dab54a5089f83e519cbdd23810f053710cd68d2
adam-lafontaine <adam.lafontiane@gmail.com>
2024-08-06 18:48:04 -0400

    reduce realloc

--- libs/usb/mem_uvc.cpp
+++ libs/usb/mem_uvc.cpp
@@ -36,14 +36,13 @@ namespace mem_uvc
 namespace mem_uvc
 {
     void* malloc(u32 n_elements, u32 element_size, cstr tag)
-    {
-        malloc_count++;
+    {       
         alloc_count++;
         auto bytes = n_elements * element_size;
         alloc_bytes += bytes;
 
+        malloc_count++;
         auto data = std::calloc(n_elements, element_size);
-
         ptr_bytes[(u64)data] = bytes;
 
         return data;
@@ -51,29 +50,34 @@ namespace mem_uvc
 
 
     void* realloc(void* ptr, u32 n_elements, u32 element_size)
-    {
-        realloc_count++;
+    {        
         auto bytes = ptr_bytes[(u64)ptr];
+        auto new_bytes = n_elements * element_size;
+
+        if (new_bytes <= bytes)
+        {
+            return ptr;
+        }
+
         alloc_bytes -= bytes;
-        bytes = n_elements * element_size;
-        alloc_bytes += bytes;
+        alloc_bytes += new_bytes;
 
-        auto data = std::realloc(ptr, bytes);
-
-        ptr_bytes[(u64)data] = bytes;
+        realloc_count++;
+        auto data = std::realloc(ptr, new_bytes);
+        ptr_bytes[(u64)data] = new_bytes;
 
         return data;
     }
 
 
     void free(void* ptr)
-    {
-        free_count++;
+    {        
         alloc_count--;
 
         auto bytes = ptr_bytes[(u64)ptr];
         alloc_bytes -= bytes;
 
+        free_count++;
         std::free(ptr);
     }
 }
